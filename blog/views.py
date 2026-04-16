1c9db0dd300546d2c43b9be6d124f511934c1748
Arvind Kharwal <karvind629@gmail.com>
2026-04-16 12:50:53 +0530

    16 April 2026

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1> Home Page of Blog App')
+    return render(request, 'blog/home.html')
 
 
 def about(request):
-    return HttpResponse('<h1> About Page of Blog App')
+    return render(request, 'blog/about.html')
