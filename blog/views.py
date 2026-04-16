195d349f2d03fbb033f8ab89e7e14dafcff0c13e
Arvind Kharwal <karvind629@gmail.com>
2026-04-16 13:05:40 +0530

    16 April 2026

--- blog/views.py
+++ blog/views.py
@@ -9,13 +9,13 @@ posts = [
         'author':'Arvind',
         'title':'Blog Post 1',
         'content':'First cntent',
-        'date-posted':'16 April 2026 ',
+        'postdate':'16 April 2026 ',
         },
         {
         'author':'Yuvaan',
         'title':'Blog Post 2',
         'content':'Second cntent',
-        'date-posted':'16 April 2026 ',
+        'postdate':'16 April 2026 ',
         }
 ]
 def home(request):
