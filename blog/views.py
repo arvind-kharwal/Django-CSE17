ebcd72b4a22bc3ce8f1be9fab69b9ce99333170b
Arvind Kharwal <karvind629@gmail.com>
2026-04-16 13:02:02 +0530

    16 April 2026

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
+posts = [
+        {
+        'author':'Arvind',
+        'title':'Blog Post 1',
+        'content':'First cntent',
+        'date-posted':'16 April 2026 ',
+        },
+        {
+        'author':'Yuvaan',
+        'title':'Blog Post 2',
+        'content':'Second cntent',
+        'date-posted':'16 April 2026 ',
+        }
+]
 def home(request):
-    return render(request, 'blog/home.html')
+    content = {
+        'posts':posts
+    }
+    return render(request,'blog/home.html',content)
 
 
 def about(request):
